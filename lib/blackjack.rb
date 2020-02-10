17fbc296d3f50c8555d459876c623d62fdd5c240
JT Lu <alohajt94@gmail.com>
2020-02-10 17:54:04 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -45,21 +45,21 @@ end
 
 def invalid_command
   puts "Please enter a valid command"
-  #prompt_user
-  #get_user_input
+  prompt_user
+  get_user_input
 end
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
-# def runner
-#   welcome #introduce the game
-#   sleep 0.75
-#   card_total = initial_round #determine your current total & tell user what number they currently have
-#   until card_total > 21 #continue game until user loses
-#     card_total = hit?(card_total)
-#     display_card_total(card_total)
-#   end
-#   end_game(card_total) #prints out once user has lost
-# end
+def runner
+  welcome #introduce the game
+  sleep 0.75
+  card_total = initial_round #determine your current total & tell user what number they currently have
+  until card_total > 21 #continue game until user loses
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total) #prints out once user has lost
+end
